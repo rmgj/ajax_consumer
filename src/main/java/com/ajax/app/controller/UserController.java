c20b3e971ddd29c227d7e8c10ded3ac98f551087
rmgj <39767686+rmgj@users.noreply.github.com>
2022-05-20 16:42:48 +0800

    Update UserController.java

--- src/main/java/com/ajax/app/controller/UserController.java
+++ src/main/java/com/ajax/app/controller/UserController.java
@@ -3,6 +3,8 @@ package com.ajax.app.controller;
 public class UserController {
     public String userReg(){
         System.out.println("---实现用户注册---");
+        System.out.println("---实现用户注册2222---");
+        
         return "index";
     }
 }
